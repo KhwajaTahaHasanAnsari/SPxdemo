2183afb45babbccf09064a21ae55c07fb0363c52
KhwajaTahaHasanAnsari <ktahahasanansari@gmail.com>
2018-06-06 18:16:45 +0500

    greet to git

---
+++ helloWorld/lib/webparts/helloWorld/HelloWorldWebPart.module.scss.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/* tslint:disable */
+require('./HelloWorldWebPart.module.css');
+var styles = {
+    helloWorld: 'helloWorld_a8a504d4',
+    container: 'container_a8a504d4',
+    row: 'row_a8a504d4',
+    column: 'column_a8a504d4',
+    'ms-Grid': 'ms-Grid_a8a504d4',
+    title: 'title_a8a504d4',
+    subTitle: 'subTitle_a8a504d4',
+    description: 'description_a8a504d4',
+    button: 'button_a8a504d4',
+    label: 'label_a8a504d4',
+};
+exports.default = styles;
+/* tslint:enable */ 
+
+//# sourceMappingURL=HelloWorldWebPart.module.scss.js.map
---
+++ helloWorld/src/webparts/helloWorld/HelloWorldWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require('./HelloWorldWebPart.module.css');
+const styles = {
+  helloWorld: 'helloWorld_a8a504d4',
+  container: 'container_a8a504d4',
+  row: 'row_a8a504d4',
+  column: 'column_a8a504d4',
+  'ms-Grid': 'ms-Grid_a8a504d4',
+  title: 'title_a8a504d4',
+  subTitle: 'subTitle_a8a504d4',
+  description: 'description_a8a504d4',
+  button: 'button_a8a504d4',
+  label: 'label_a8a504d4',
+};
+
+export default styles;
+/* tslint:enable */
